0d06caf360445021bf92d7819b9acd0f68601db1
Anil Kulkarni <anil@terminal.space>
2023-02-25 11:00:11 -0800

    Add default export to parse a GeoJSON file

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ import Track from './track.js';
 import Line from './line.js';
 
 export { Track, Line };
+
+const parse = (geoJSON) => {
+  const features = geoJSON.features ?? [];
+  return features
+    .map((feature) => {
+      switch (feature.geometry?.type) {
+        case 'LineString':
+          return new Track(feature);
+      }
+    })
+    .filter((x) => x);
+};
+export default parse;
